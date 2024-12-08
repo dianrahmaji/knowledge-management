72bfcfd4e6748283d336f08c63a0359770a63829
Dian Rahmaji <dianrahmaji@gmail.com>
2024-12-08 21:52:04 +0700

    feat(dashboard): setup files

---
+++ apps/dashboard/src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <div>Hello "__root"!</div>
+      <Outlet />
+      <TanStackRouterDevtools />
+      <ReactQueryDevtools />
+    </>
+  );
+}
